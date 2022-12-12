1d84b5316698e73a880f8e0d43593590d3b584d9
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-12 12:27:49 +0530

    addemployee

--- src/main/java/com/example/NestLmsBackend/controller/EmployeeController.java
+++ src/main/java/com/example/NestLmsBackend/controller/EmployeeController.java
@@ -1,16 +1,29 @@
 package com.example.NestLmsBackend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.NestLmsBackend.dao.EmployeeDao;
+import com.example.NestLmsBackend.model.EmployeeModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    EmployeeDao dao;
     @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String Homepage(){
         return "Welcome to my website";
+    }
+
+    @PostMapping(path = "/addemployee", consumes = "application/json", produces = "application/json")
+    public HashMap<String,String> AddEmployee(@RequestBody EmployeeModel e){
+        HashMap<String,String> map=new HashMap<>();
+        dao.save(e);
+        map.put("status","success");
+        return map;
     }
 
 }
