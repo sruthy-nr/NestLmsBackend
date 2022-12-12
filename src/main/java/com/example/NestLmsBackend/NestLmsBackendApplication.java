5b1fc44cc7cbb7d311cbb50c71e4250d7106feab
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-12 11:56:26 +0530

    master

---
+++ src/main/java/com/example/NestLmsBackend/NestLmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestLmsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestLmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestLmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestLmsBackend/NestLmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestLmsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestLmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
