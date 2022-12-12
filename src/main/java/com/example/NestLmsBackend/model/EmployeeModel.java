60c6e7f735ef798a85f0de5d3d1f928aa16b270c
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-12 12:12:26 +0530

    employeemodel

---
+++ src/main/java/com/example/NestLmsBackend/model/EmployeeModel.java
@@ -0,0 +1,99 @@
+package com.example.NestLmsBackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class EmployeeModel {
+    @Id
+    @GeneratedValue
+    private int id;
+    private int empcode;
+    private String name;
+    private String designation;
+    private int salary;
+    private String phone;
+    private String email;
+    private String password;
+
+    public EmployeeModel() {
+    }
+
+    public EmployeeModel(int id, int empcode, String name, String designation, int salary, String phone, String email, String password) {
+        this.id = id;
+        this.empcode = empcode;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmpcode() {
+        return empcode;
+    }
+
+    public void setEmpcode(int empcode) {
+        this.empcode = empcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
